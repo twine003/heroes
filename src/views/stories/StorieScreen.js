6c0b5277bdc3c39714d71f252c572597dc7494a1
jwgarcia003 <jwgarcia003@gmail.com>
2021-02-08 17:04:07 -0600

    first commit

--- src/views/stories/StorieScreen.js
+++ src/views/stories/StorieScreen.js
@@ -48,20 +48,44 @@ const StorieScreen = () =>{
                                         <CRow>
                                             {
                                                 single_storie?.comics?.items?.map(comic=>(
-                                                    <CCol sm="12" md="3" lg="4">
+                                                    <CCol sm="12" md="6" lg="6">
                                                         <div className="h3  text-info">{comic.title}</div> 
-                                                        <div className="h5  text-dark">Page count: {comic.pageCount}</div> 
-                                                        <div className="text-success">Print price: $ {comic.prices[0]?.price}</div> 
-                                                        <div className="text-success">Digital price: $ {comic.prices[1]?.price}</div> 
-
                                                         
-                                                        <CImg
-                                                            src={comic?.thumbnail?.path + '.jpg'}
-                                                            fluid
-                                                            thumbnail
-                                                            className="mb-2"
-                                                            />
+                                                        
+                                                        <CRow>
+                                                            <CCol sm="12" md="6" lg="6">
+                                                                <CImg
+                                                                src={comic?.thumbnail?.path + '.jpg'}
+                                                                fluid
+                                                                thumbnail
+                                                                className="mb-2"
+                                                                style={{ maxWidth: "300px"}}
+                                                                />
+                                                            </CCol>
+                                                            <CCol sm="12" md="6" lg="6">
+                                                                <strong>Characters:</strong>
+                                                                {
+                                                                    comic.characters?.items.map(c=>(
+                                                                        <span>{c.name}; </span> 
+                                                                    ))
+                                                                }
+                                                                <hr></hr>
+                                                                <strong>Creators:</strong>
+                                                                {
+                                                                    comic.creators?.items.map(c=>(
+                                                                        <span>{c.name}; </span> 
+                                                                    ))
+                                                                }
+                                                                
+                                                                <div className="h5  text-dark">Page count: {comic.pageCount}</div> 
+                                                                <div className="text-success">Print price: $ {comic.prices[0]?.price}</div> 
+                                                                <div className="text-success">Digital price: $ {comic.prices[1]?.price}</div> 
+                                                            </CCol>
+                                                        </CRow>
+                                                        
+                                                        
                                                     </CCol>
+                                                    
                                                 ))
                                             }
                                         </CRow>
@@ -78,7 +102,7 @@ const StorieScreen = () =>{
                         <CCol>
                             <CCard>
                                 <CCardBody>
-                                <div className="h1 text-danger">Historia no encontrada :(</div> 
+                                    <div className="h1 text-danger">Historia no encontrada :(</div> 
                                 </CCardBody>
                                 
                             </CCard>
